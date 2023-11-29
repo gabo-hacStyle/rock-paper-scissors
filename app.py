678178e4da77b2dcfc2c31c1b6dfa8b0c8fb970c
Ćompu Grande -- Gabriel <gabo2023brazil@gmail.com>
2023-11-29 16:53:24 -0500

    Improving the visual of the game and re-commenting

--- app.py
+++ app.py
@@ -1,51 +1,72 @@
+# Import the random module for generating random choices
 import random
 
-#valid choices as a tuple  
+# Define the valid choices for the game as a tuple
 valid_choices = ("rock", "paper", "scissors")
-#Count to see who is winning
+
+# Initialize counters for the number of wins by the player, the computer, and ties
 player_wins = 0
 computer_wins = 0
 ties = 0
 
+# Ask the user for the number of rounds they want to play
 rounds = input("How many rounds do you want to play? ")
 
+# Function to get the player's choice
 def get_player_choice():
-        player_choice = input("Enter your choice (rock, paper, scissors): ").lower()
-        while player_choice not in valid_choices:
-            player_choice = input("Invalid choice. Please enter rock, paper, or scissors: ").lower()
-        return player_choice
+    # Ask the player for their choice
+    player_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+    # If the player's choice is not valid, ask again until a valid choice is made
+    while player_choice not in valid_choices:
+        player_choice = input("Invalid choice. Please enter rock, paper, or scissors: ").lower()
+    return player_choice
 
+# Function to get the computer's choice
 def get_computer_choice():
-        return random.choice(valid_choices)
+    # The computer's choice is a random selection from the valid choices
+    return random.choice(valid_choices)
 
+# Function to determine the winner of a round
 def determine_winner(player_choice, computer_choice):
-        if player_choice == computer_choice:
-            global ties
-            ties += 1
-            return "It's a tie!"
-        if (player_choice == "rock" and computer_choice == "scissors") or \
-        (player_choice == "scissors" and computer_choice == "paper") or \
-        (player_choice == "paper" and computer_choice == "rock"):
-            global player_wins
-            player_wins += 1
-            
-            return "You win!"
-        else:
-            global computer_wins
-            computer_wins += 1
-            return "You lose!"
-def play_game():
-    player_choice = get_player_choice()
-    computer_choice = get_computer_choice()
-    result = determine_winner(player_choice, computer_choice)
-    print(f"You chose {player_choice}, computer chose {computer_choice}. {result}")
-
-def end_game():
-    print('--' *20)
+    # If the player's choice is the same as the computer's, it's a tie
+    if player_choice == computer_choice:
+        global ties
+        ties += 1
+        return "It's a tie!"
+    # Define the conditions for the player to win
+    if (player_choice == "rock" and computer_choice == "scissors") or \
+    (player_choice == "scissors" and computer_choice == "paper") or \
+    (player_choice == "paper" and computer_choice == "rock"):
+        global player_wins
+        player_wins += 1
+        return "You win!"
+    # If none of the above conditions are met, the computer wins
+    else:
+        global computer_wins
+        computer_wins += 1
+        return "You lose!"
+    
+#Function to print the scores
+def print_scores():
     print('--' *20)
     print('Computer Score: ', computer_wins)
     print('Player Score: ', player_wins)
     print('Ties: ', ties)
+
+# Function to play a round of the game
+def play_game():
+    player_choice = get_player_choice()
+    print(' ' * 20)
+    computer_choice = get_computer_choice()
+    result = determine_winner(player_choice, computer_choice)
+    print(f"-->> You chose {player_choice}, computer chose {computer_choice}. {result}")
+    print(' ' * 20)
+
+# Function to handle the end of the game
+def end_game():
+    print('--' *20)
+    print('End of the gamee!!'.upper())
+    print_scores()
     if player_wins > computer_wins:
         print("You won the game!")
     elif computer_wins > player_wins:
@@ -54,20 +75,22 @@ def end_game():
         print("The game was a tie!")
     print('--' *20)
     print('--' *20)
-     
+
+# Play the game for the number of rounds specified by the user
 for round in range(int(rounds)):
-    print('--' *20)
-    print('Computer Score: ', computer_wins)
-    print('Player Score: ', player_wins)
-    print('Ties: ', ties)
+    print_scores()
     print('--' *20)
     print(f"Round {round + 1}")
     print('--' *20)
+    #Function to play the game
     play_game()
+    # If it's the last round, end the game
     if round == int(rounds) - 1:
         end_game()
+    # If it's not the last round, ask the user if they want to continue playing
     else:
-        user_continue = input("Do you want to play again? (y/n): ").lower()
-        if user_continue != "y":
+        user_continue = input("Quit game? (y/N): ")
+        # If the user doesn't want to continue, end the game and break the loop
+        if user_continue == "y":
             end_game()
             break
