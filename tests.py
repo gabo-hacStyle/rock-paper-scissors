01b97db9579d3e814c82a769b239694c07468c04
Ćompu Grande -- Gabriel <gabo2023brazil@gmail.com>
2023-11-29 16:55:50 -0500

    Commenting tests.py

--- tests.py
+++ tests.py
@@ -1,21 +1,37 @@
-# Description: This file contains the test cases for the application.
-#Create a matrix of 3x3 and test the functions
+# This file contains the test cases for the application.
 
-matrix = [[1,2,32, 234, 45, 56,23, 34, 34, 34, 332, 43, 54, 33],[47,352,634],[73,18,29]]  #matrix of 3x3
+# Create a 3x3 matrix for testing
+matrix = [[1,2,32, 234, 45, 56,23, 34, 34, 34, 332, 43, 54, 33],[47,352,634],[73,18,29]]
 print('Matriz ->', matrix)
 
+# Print a separator for clarity
 print('--' * 20)
+
+# Print the odd numbers in the matrix
 print("Numeros impares de la matriz")
 print('--' * 20)
+
+# Iterate over each element in the matrix
 for element in matrix:
+    # Iterate over each item in the element
     for item in element:
+        # If the item is odd (remainder of division by 2 is not 0)
         if item % 2 != 0:
+            # Print the item and its position in the matrix
             print(item, "esta en la posicion =>", "(", matrix.index(element), ",", element.index(item), ")") 
+
+# Print a separator for clarity
 print('--' * 20)
+
+# Print the even numbers in the matrix
 print("Numeros pares de la matriz")
 print('--' * 20)
 
+# Iterate over each element in the matrix
 for element in matrix:
+    # Iterate over each item in the element
     for item in element:
+        # If the item is even (remainder of division by 2 is 0)
         if item % 2 == 0:
+            # Print the item and its position in the matrix
             print(item, "esta en la posicion =>", "(", matrix.index(element), ",", element.index(item), ")")
